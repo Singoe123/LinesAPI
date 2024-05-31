bbc05d020c35518fb78261eadd740c70f5ebf363
Singoe123 <marcelo.poggi@hotmail.com>
2024-05-31 03:23:55 -0500

    fully dockerized mongodb deploy
    details:
    
    - dockerfile was created to set up initialization scripts
    - docker-compose file was created to deploy multiple images at the same
      time
    - volume was set up to avoid information loss when swaping containers

---
+++ mongodb/docs.js
@@ -0,0 +1,118 @@
+use("lines")
+
+// Insert test documents in the "users" collection
+db.users.insertMany([
+  {
+    username: "user1",
+    name: "User One",
+    email: "user1@example.com",
+    password: "password1",
+    follower_count: 10,
+    profilePic: {
+      url: "https://example.com/user1.jpg",
+      size: 1024,
+      width: 200,
+      height: 200
+    },
+    bio: "I am User One"
+  },
+  {
+    username: "user2",
+    name: "User Two",
+    email: "user2@example.com",
+    password: "password2",
+    follower_count: 20,
+    profilePic: {
+      url: "https://example.com/user2.jpg",
+      size: 2048,
+      width: 300,
+      height: 300
+    },
+    bio: "I am User Two"
+  },
+]);
+
+// Insert test documents in the "posts" collection
+db.posts.insertMany([
+  {
+    content: "This is post 1",
+    author: ObjectId("60a6e4e0e8e8e8e8e8e8e8e8"),
+    publication_date: new Date(),
+    stats: {
+      likes: 5,
+      responses: 2,
+      reposts: 1
+    }
+  },
+  {
+    content: "This is post 2",
+    author: ObjectId("60a6e4e0e8e8e8e8e8e8e8e8"),
+    publication_date: new Date(),
+    stats: {
+      likes: 10,
+      responses: 3,
+      reposts: 2
+    }
+  },
+]);
+
+// Get the user and post IDs to use in the likes and reposts collections
+
+const userIds = []
+
+const users = db.users.find({}).toArray()
+for (const user of users) {
+  userIds.push(user._id)
+}
+
+const postIds = []
+
+const posts = db.posts.find({}).toArray()
+for (const post of posts) {
+  postIds.push(post._id)
+}
+
+// Insert test documents in the "likes" collection
+
+const likes = []
+
+for (let i = 0; i < userIds.length; i++) {
+  const like = {
+    user: userIds[i],
+    post: postIds[i],
+    like_date: new Date()
+  }
+  likes.push(like)
+}
+
+db.likes.insertMany(likes);
+
+// Insert test documents in the "reposts" collection
+
+const reposts = []
+
+for (let i = 0; i < userIds.length; i++) {
+  const like = {
+    user: userIds[i],
+    post: postIds[i],
+    repost_date: new Date()
+  }
+  reposts.push(like)
+}
+
+db.reposts.insertMany(reposts);
+
+// Insert test documents in the "followers" collection
+
+const followers = [{
+  follower: userIds[0],
+  following: userIds[1],
+  follow_date: new Date()
+},
+{
+  follower: userIds[1],
+  following: userIds[0],
+  follow_date: new Date()
+},]
+
+db.followers.insertMany(followers);
---
+++ mongodb/init.js
@@ -0,0 +1,227 @@
+console.log('starting script');
+
+try{
+
+  
+  use('lines');
+
+db.createCollection("users", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      required: ["username", "name", "email", "password", "follower_count"],
+      properties: {
+        username: {
+          bsonType: "string",
+          pattern: "^[1-9a-zA-Z_]+$",
+          description: "must be a string and contain only alphanumerics and underscores",
+          minLength: 4,
+          maxLength: 15
+        },
+        name: {
+          bsonType: "string",
+          description: "must be a string and is required",
+          maxLength: 50
+        },
+        email: {
+          bsonType: "string",
+          pattern: "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",
+          description: "must be a string and match the email pattern"
+        },
+        password: {
+          bsonType: "string",
+          description: "must be a string and is required"
+        },
+        follower_count: {
+          bsonType: "int",
+          minimum: 0,
+          description: "must be an int and is required"
+        },
+        profilePic: {
+          bsonType: "object",
+          required: ["url", "size", "width", "height"],
+          properties: {
+            url: {
+              bsonType: "string",
+              pattern: "^(https?|ftp)://[^\\s/$.?#].[^\\s]*$",
+              description: "must be a valid URI and is required"
+            },
+            size: {
+              bsonType: "int",
+              description: "must be an int and is required"
+            },
+            width: {
+              bsonType: "int",
+              description: "must be an int and is required"
+            },
+            height: {
+              bsonType: "int",
+              description: "must be an int and is required"
+            }
+          }
+        },
+        bio: {
+          bsonType: "string",
+          description: "must be a string and is required"
+        }
+      }
+    }
+  }
+});
+
+db.createCollection("posts", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      required: ["content", "author", "publication_date", "stats"],
+      properties: {
+        content: {
+          bsonType: "string",
+          description: "must be a string and is required",
+          maxLength: 280
+        },
+        author: {
+          bsonType: "objectId",
+          description: "must be an objectId referencing the user and is required"
+        },
+        parent_post: {
+          bsonType: "objectId",
+          description: "optional reference to the parent post if this is a response"
+        },
+        publication_date: {
+          bsonType: "date",
+          description: "must be a date and is required"
+        },
+        stats: {
+          bsonType: "object",
+          required: ["likes", "responses", "reposts"],
+          properties: {
+            likes: {
+              bsonType: "int",
+              minimum: 0,
+              description: "must be an int and is required"
+            },
+            responses: {
+              bsonType: "int",
+              minimum: 0,
+              description: "must be an int and is required"
+            },
+            reposts: {
+              bsonType: "int",
+              minimum: 0,
+              description: "must be an int and is required"
+            }
+          }
+        }
+      }
+    }
+  }
+});
+
+db.createCollection("likes",{
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      required: ["user", "post", "like_date"],
+      properties:{
+        user: {
+          bsonType: "objectId",
+          description: "must be an objectId referencing the user and is required"
+        },
+        post: {
+          bsonType: "objectId",
+          description: "must be an objectId referencing the post and is required"
+        },
+        like_date: {
+          bsonType: "date",
+          description: "must be a date and is required"
+        }
+      }
+    }
+  }
+});
+
+db.createCollection("reposts", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      required: ["user", "post", "repost_date"],
+      properties: {
+        user: {
+          bsonType: "objectId",
+          description: "must be an objectId referencing the user and is required"
+        },
+        post: {
+          bsonType: "objectId",
+          description: "must be an objectId referencing the post and is required"
+        },
+        repost_date: {
+          bsonType: "date",
+          description: "must be a date and is required"
+        }
+      }
+    }
+  }
+});
+
+db.createCollection("followers", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      required: ["follower", "following", "follow_date"],
+      properties: {
+        follower: {
+          bsonType: "objectId",
+          description: "must be an objectId referencing the user and is required"
+        },
+        following: {
+          bsonType: "objectId",
+          description: "must be an objectId referencing the user and is required"
+        },
+        follow_date: {
+          bsonType: "date",
+          description: "must be a date and is required"
+        }
+      }
+    }
+  }
+});
+
+
+
+// Indexes for users collection
+db.users.createIndex({ username: 1 });
+
+// Indexes for posts collection
+db.posts.createIndex({ parent_post: 1 });
+db.posts.createIndex({ "stats.likes": -1 });
+db.posts.createIndex({ "stats.responses": -1 });
+db.posts.createIndex({ "stats.reposts": -1 });
+db.posts.createIndex({ author: 1, publication_date: -1 });
+
+// Indexes for likes collection
+db.likes.createIndex({ user: 1 });
+db.likes.createIndex({ post: 1 });
+
+// Indexes for reposts collection
+db.reposts.createIndex({ user: 1, post: 1 });
+db.reposts.createIndex({ repost_date: -1 });
+db.reposts.createIndex({ user: 1 });
+db.reposts.createIndex({ post: 1 });
+
+// Indexes for followers collection
+db.followers.createIndex({ follower: 1 });
+db.followers.createIndex({ following: 1 });
+db.followers.createIndex({ follower: 1, following: 1 });
+db.followers.createIndex({ follow_date: -1 });
+} catch (e) {
+  console.log(e);
+}
+
+console.log('finished script');
+
+
+
+
+
+
